0b8f73caf175fc37e2fed2af8026440840748244
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2025-07-29 01:52:25 +0530

    Fixed spelling mistake on import statement.

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { sendOtp, verifyOtpAndSignUp } from "../controllers/authController.js";
+import { sendOtp, verifyOtp } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/send-otp", sendOtp);
-router.post("/verify-otp", verifyOtpAndSignUp);
+router.post("/verify-otp", verifyOtp);
 
 
 
