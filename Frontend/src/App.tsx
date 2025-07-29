c32dfe8d3a8348bfc6f56dc18ab6507b9ae13acb
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2025-07-29 19:40:40 +0530

    Fixing send-otp logic.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,21 +2,22 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Signup from "./components/Signup/Signup";
 // import Signin from "./components/Signin/Signin";
-import Account from "./components/Account/Account";
+import Account from "./components/Account/Account.tsx";
 import Note from "./components/Note/Note.tsx";
+
 
 function App() {
   return (
     <>
       {/* <div> */}
-        <Router>
-          <Routes>
-            <Route path="/" element={ <Account /> } />
-            <Route path="/Note" element={ <Note /> } />
-            {/* <Route path="/Signup" element={ <Signup /> } />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Account />} />
+          <Route path="/Note" element={<Note />} />
+          {/* <Route path="/Signup" element={ <Signup /> } />
             <Route path="/Signin" element={ <Signin /> } /> */}
-          </Routes>
-        </Router>
+        </Routes>
+      </Router>
       {/* </div> */}
     </>
   );
